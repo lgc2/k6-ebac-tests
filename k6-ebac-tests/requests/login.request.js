1b466a13f1de3ca8a9ea5cfad85d46e458860419
luiz.carvalho <luiz.carvalho@cwi.com.br>
2023-03-19 18:51:12 -0300

    primeiro commit

---
+++ k6-ebac-tests/requests/login.request.js
@@ -0,0 +1,29 @@
+import { check } from "k6"
+import http from "k6/http"
+import Utils from "../utils/utils"
+
+export default class Login {
+    #token
+
+    access(user, password) {
+        let response = http.post(`${Utils.getBaseUrl()}/login`, JSON.stringify(
+            {
+                "username": user,
+                "password": password
+            }
+        ), {
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            }
+        })
+        this.#token = response.json('accessToken')
+        check(response, {
+            "is status 201": (r) => r.status === 201
+        })
+    }
+
+    getToken() {
+        return this.#token
+    }
+}
